41bd4f44ce5209755fa956d0146c819cdea74c97
cscott7240 <cscott9864@gmail.com>
2020-04-15 13:56:51 -0500

    trying diffrent things to get JavaScript to work

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-var cost_input = document.querySelector('.cost');
-var service_input = document.querySelector('.service');
-var people_input = document.querySelector('.people');
+const cost_input = document.querySelector('.cost');
+const service_input = document.querySelector('.service');
+const people_input = document.querySelector('.people');
 
-var tip_btn = document.querySelector('#tip-btn');
+const tip_btn = document.querySelector('#tip-btn');
 
 tip_btn.addEventListener('click', calc_tip);
 
-Function calc_tip () {
-var cost = Number(cost_input.value);
- var service = Number(service_input.value) / 100;
-var people = Number(people_input.value);
-var tip = (cost * service) / people;
+function calc_tip () {
+ let cost = Number(cost_input.value);
+ let service = Number(service_input.value) / 100;
+ let people = Number(people_input.value);
+ let tip = (cost * service) / people;
  
  let tip_el = document.querySelector('.tip-wrap .tip');
  tip_el.innerText = '$' + tip.toFixed(2) + ((people > 1) ? ' each' : '');
